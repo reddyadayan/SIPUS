cdeb0f8c7430638ad995576dc8e115ced20f40e5
reddyadayan <reddya@undiksha.ac.id>
2023-01-08 15:43:28 +0000

    Code Synced by DhiWise

---
+++ app/src/test/java/com/sipus/app/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.sipus.app
+
+import com.sipus.app.appcomponents.base.BaseActivity
+import com.sipus.app.databinding.LayoutProgressDialogBinding
+
+class MainActivity : BaseActivity<LayoutProgressDialogBinding>(R.layout.layout_progress_dialog) {
+
+    override fun onInitialized() {
+
+    }
+
+    override fun setUpClicks() {
+
+    }
+}
